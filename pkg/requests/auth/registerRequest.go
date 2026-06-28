d5c5be6fecb50159a122d04481e7e8539b55f78d
Ck Yong <118044421+ckyong826@users.noreply.github.com>
2026-06-28 21:07:09 +0800

    feat: Update RegisterRequest to require username and phone number fields

--- pkg/requests/auth/registerRequest.go
+++ pkg/requests/auth/registerRequest.go
@@ -5,8 +5,8 @@ import "swapngo-backend/internal/models"
 type RegisterRequest struct {
 	Email       string             `json:"email" binding:"required,email"`
 	Password    string             `json:"password" binding:"required"`
-	Username    string             `json:"username" binding:"omitempty"`
-	PhoneNumber string             `json:"phone_number" binding:"omitempty"`
+	Username    string             `json:"username" binding:"required,min=3"`
+	PhoneNumber string             `json:"phone_number" binding:"required,e164"`
 	Pin         string             `json:"pin" binding:"required,len=4,numeric"`
 	AccountName string             `json:"account_name" binding:"omitempty"`
 	CustodyType models.CustodyType `json:"custody_type" binding:"omitempty"`
