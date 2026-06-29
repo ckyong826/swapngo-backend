15c0445175da23368a7b1a4598170b70dacd86e6
Ck Yong <118044421+ckyong826@users.noreply.github.com>
2026-06-29 19:37:13 +0800

    feat: Add SuiTxHash field to DepositResponse and update ToDepositResponse function

--- pkg/responses/transaction/deposit_response.go
+++ pkg/responses/transaction/deposit_response.go
@@ -11,13 +11,14 @@ type DepositResponse struct {
 	AmountMYR         float64 `json:"amount_myr"`
 	MYRCMinted        float64 `json:"myrc_minted"`
 	BillplzPaymentURL string  `json:"billplz_payment_url"`
+	SuiTxHash         *string `json:"sui_tx_hash"`
 	ErrorMessage      *string `json:"error_message"`
 	CreatedAt         string  `json:"created_at"`
 	UpdatedAt         string  `json:"updated_at"`
 }
 
 func ToDepositResponse(d *models.Deposit) DepositResponse {
-	return DepositResponse{
+	r := DepositResponse{
 		ID:                d.ID.String(),
 		Status:            utils.NormalizeStatus(d.Status),
 		AmountMYR:         d.AmountMYR,
@@ -26,6 +27,10 @@ func ToDepositResponse(d *models.Deposit) DepositResponse {
 		CreatedAt:         d.CreatedAt.UTC().Format("2006-01-02T15:04:05Z"),
 		UpdatedAt:         d.UpdatedAt.UTC().Format("2006-01-02T15:04:05Z"),
 	}
+	if d.TxHash != "" {
+		r.SuiTxHash = &d.TxHash
+	}
+	return r
 }
 
 func ToDepositResponses(deposits []*models.Deposit) []DepositResponse {
